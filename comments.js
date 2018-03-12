9c31505164b6bcc859230c271079c4a4eb91b147
danielld75 <danielld@poczta.fm>
2018-03-12 22:09:14 +0100

    First commit

---
+++ comments.js
@@ -0,0 +1,54 @@
+import {ADD_COMMENT, EDIT_COMMENT, REMOVE_COMMENT, THUMB_DOWN_COMMENT, THUMB_UP_COMMENT} from "./actions";
+
+const initialState = {
+  comments: [],
+  users: []
+};
+
+export default function comments(state = initialState, action) {
+  switch(action.type) {
+    case ADD_COMMENT:
+      return Object.assign({}, state, {
+        comments: [
+          {
+            id: action.id,
+            text: action.text,
+            votes: 0
+          }
+          , ...state]
+      });
+    case REMOVE_COMMENT:
+      return Object.assign({}, state, {
+        comments: state.comments.filter(comment => comment.id !== action.id)
+      });
+    case EDIT_COMMENT:
+      return Object.assign({}, state, {
+        comments: state.comments.map(comment => {
+          if(comment.id === action.id){
+            return {text: action.text, ...comment}
+          }
+          return comment;
+        })
+      });
+    case THUMB_UP_COMMENT:
+      return Object.assign({}, state, {
+        comments: state.comments.map(comment => {
+          if(comment.id === action.id) {
+            return {...comment, votes: comment.votes + 1}
+          }
+          return comment;
+        })
+      });
+    case THUMB_DOWN_COMMENT:
+      return Object.assign({}, state, {
+        comments: state.comments.map(comment => {
+          if(comment.id === action.id) {
+            return {...comment, votes: comment.votes - 1}
+          }
+          return comment;
+        })
+      });
+    default:
+      return state;
+  }
+}
